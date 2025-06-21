93562aaa0894e1248e4b10a7472f8a783fa392a7
polecoder <maurop4502@gmail.com>
2025-06-20 23:36:09 -0300

    primeros pasos con Godot: movimiento WASD básico

---
+++ player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+const SPEED = 100
+
+func _physics_process(_delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	velocity = input_vector * SPEED
+	move_and_slide()
